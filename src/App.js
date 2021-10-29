a172dfd6a74044f4feb35df58b1b3c4832428aa3
AleksDu <aleksandr.dyshkin@gmail.com>
2021-10-29 13:51:35 +0300

    35.5 Marshrytizaciya

--- src/App.js
+++ src/App.js
@@ -1,79 +1,71 @@
-import { useState, useCallback, useMemo } from "react";
-import { useLS } from "./Hooks/useLS";
-import { useToggle } from "./Hooks/useToggle";
+import { lazy, Suspense } from "react";
 
-import { ProductList } from "./Components/Products/ProductList";
-import { Modal } from "./Components/Modal/Modal";
-import { Form } from "./Components/Forms/Form";
-import { SearchFrom } from "./views/PexelsImages/SearchForm";
-import { ImagesList } from "./views/PexelsImages/ImagesList";
 import "./App.css";
+import { NavLink, Route, Switch } from "react-router-dom";
+import { Loader } from "./Components/Loader/Loader";
+const HomePage = lazy(() =>
+  import("./pages/Home/Home" /* webpackChangName: Home Page   */)
+);
+const PexelsPage = lazy(() =>
+  import("./pages/Pexels/Pexels" /* webpackChangName: Pexels Page   */)
+);
+const ProductsPage = lazy(() =>
+  import("./pages/Products/Products" /* webpackChangName: Products Page   */)
+);
 // import { SolidTitle } from './components/Titles/SolidTitle';
 function App() {
   //   const [counter, setCounter] = useState(0);
   //   const [isOpen, setIsOpen] = useState(false);
   // === свои хуки
-  const [searchValue, setSearchValue] = useState("");
-  const [showModal, setShowModal] = useToggle(false);
+  // const [searchValue, setSearchValue] = useState("");
+
   // === свои хуки
 
   // - список
-  const [filter, setFilter] = useState("");
-  const [allProducts, setAllProducts] = useLS("products", []);
-  const filteredProducts = useMemo(() => {
-    let normFilter = filter.toLowerCase();
-    return allProducts.filter((prod) =>
-      prod.title.toLowerCase().includes(normFilter)
-    );
-  }, [filter, allProducts]);
+
   // - список
-
-  const [perPage, setPerPage] = useState(5);
-  const getSearchValues = (searchValue, perPage) => {
-    setSearchValue(searchValue);
-    setPerPage(perPage);
-  };
-
-  const addNewProduct = (obj) => setAllProducts((zuzuzu) => [...zuzuzu, obj]);
-  const deleteProduct = (id) =>
-    setAllProducts((zuzuzu) => zuzuzu.filter((prod) => prod.id !== id));
-
-  const handleChangeFilter = useCallback((e) => setFilter(e.target.value), []);
-
-  // console.log('filteredProducts', filteredProducts);
-  // console.log('showModal', setShowModal);
 
   return (
     <div className="App">
-      <SearchFrom getSearchValues={getSearchValues} />
-      <ImagesList searchValue={searchValue} perPage={perPage} />
+      <nav>
+        <li>
+          <NavLink
+            exact
+            to="/"
+            className="naVLink"
+            activeClassName="activeNavLink"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/pexels"
+            className="naVLink"
+            activeClassName="activeNavLink"
+          >
+            Pexels
+          </NavLink>
+          <NavLink
+            to="/products"
+            className="naVLink"
+            activeClassName="activeNavLink"
+          >
+            Products
+          </NavLink>
+        </li>
+      </nav>
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
 
-      {showModal && (
-        <Modal toggleModal={setShowModal}>
-          <Form addNewProduct={addNewProduct} />
-        </Modal>
-      )}
-      <h1>FE-35 Product</h1>
-      {/* <SolidTitle titleText="FE-35 Product" /> */}
-      <button type="button" onClick={setShowModal}>
-        Add Product
-      </button>
-      {/* === FILTER СПИСКА ПРОДУКТОВ === */}
-      <br />
-      <label htmlFor="filter">Filter</label>
-      <br />
-      <input
-        type="text"
-        id="filter"
-        value={filter}
-        onChange={handleChangeFilter}
-      />
-      <br />
-      {/* === РЕНДЕР КОМПОНЕНТА СПИСКА ПРОДУКТОВ === */}
-      <ProductList
-        products={filteredProducts}
-        onDeleteProduct={deleteProduct}
-      />
+          <Route path="/pexels">
+            <PexelsPage title="Main Title" />
+          </Route>
+          <Route path="/products" component={ProductsPage} />
+          <Route>
+            <p>Page not found </p>
+          </Route>
+        </Switch>
+      </Suspense>
     </div>
   );
 }
